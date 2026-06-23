23fab290e61cf38832c36b9f931f1e8f955ae13e
Yasmin Serag <yasminserag2008@gmail.com>
2026-06-23 23:04:09 +0300

    Add basic benchmark script

--- common/apps/flaskapp.py
+++ common/apps/flaskapp.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+import time
 
 flask_app = Flask(__name__)
 
@@ -12,6 +13,11 @@ def status():
         "status": "online",
         "framework": "Flask"
     })
+
+@flask_app.route('/slow')
+def slow():
+    time.sleep(0.1)
+    return "done"
 
 # Expose the standard WSGI callable interface for my server instead of using flask run
 app = flask_app.wsgi_app
